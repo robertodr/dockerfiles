8de0a26fa19761c59332557e7e97a87f30094f0e
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-04-01 10:46:57 -0400

    Revamp caching

--- dependencies/download_dependencies.sh
+++ dependencies/download_dependencies.sh
@@ -4,34 +4,37 @@
 set -euo pipefail
 
 echo "-- Downloading PGI 17.4"
-if [[ -f $HOME/Downloads/pgilinux-2017-174_x86_64/linux86-64.examples.tar.gz ]]; then
+if [[ -f $HOME/Downloads/pgilinux/install_components/linux86-64.examples.tar.gz ]]; then
   echo "-- PGI 17.4 FOUND in cache"
 else
   echo "-- PGI 17.4 NOT FOUND in cache"
   cd $HOME/Downloads
-  curl -Ls $PGI_URL | tar -xz -C pgilinux-2017-174-x86_64 --strip-components=1
+  mkdir -p pgilinux
+  curl -Ls $PGI_URL | tar -xz -C pgilinux
   cd $TRAVIS_BUILD_DIR
 fi
 echo "-- Done with PGI 17.4"
 
 echo "-- Downloading MKL"
-if [[ -f $HOME/Downloads/l_mkl_2017.4.239/ipsxe_support_mkl.txt ]]; then
+if [[ -f $HOME/Downloads/mkl/ipsxe_support_mkl.txt ]]; then
   echo "-- MKL FOUND in cache"
 else
   echo "-- MKL NOT FOUND in cache"
   cd $HOME/Downloads
-  curl -Ls $MKL_URL | tar -xz -C l_mkl_2017.4.239 --strip-components=1
+  mkdir -p mkl
+  curl -Ls $MKL_URL | tar -xz -C mkl --strip-components=1
   cd $TRAVIS_BUILD_DIR
 fi
 echo "-- Done with MKL"
 
 echo "-- Downloading Intel"
-if [[ -f $HOME/Downloads/parallel_studio_xe_2018_update2_professional_edition/ipsxe_support_prof.txt ]]; then
+if [[ -f $HOME/Downloads/parallel_studio/ipsxe_support_prof.txt ]]; then
   echo "-- Intel FOUND in cache"
 else
   echo "-- Intel NOT FOUND in cache"
   cd $HOME/Downloads
-  curl -Ls $INTEL_URL | tar -xz -C parallel_studio_xe_2018_update2_professional_edition --strip-components=1
+  mkdir -p parallel_studio
+  curl -Ls $INTEL_URL | tar -xz -C parallel_studio --strip-components=1
   cd $TRAVIS_BUILD_DIR
 fi
 echo "-- Done with Intel"
@@ -44,7 +47,8 @@ if [[ -f $HOME/Downloads/hdf5-1.10.1/COPYING_LBNL_HDF5 ]]; then
 else
   echo "-- HDF5 $HDF5_VERSION NOT FOUND in cache"
   cd $HOME/Downloads
-  curl -Ls $HDF5_URL | tar -xj -C hdf5-1.10.1
+  mkdir -p hdf5-1.10.1
+  curl -Ls $HDF5_URL | tar -xj -C hdf5-1.10.1 --strip-components=1
   cd $TRAVIS_BUILD_DIR
 fi
 echo "-- Done with HDF5 $HDF5_VERSION"
