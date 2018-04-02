31ee7867d2a3b7ba9480a49006f937fc335c1071
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-04-02 12:59:41 -0400

    Rebuild cache differently

--- dependencies/download_dependencies.sh
+++ dependencies/download_dependencies.sh
@@ -4,37 +4,37 @@
 set -euo pipefail
 
 echo "-- Downloading PGI 17.4"
-if [[ -f $HOME/Downloads/pgilinux/install_components/linux86-64.examples.tar.gz ]]; then
+if [[ -f $HOME/Downloads/pgilinux.tar.gz ]]; then
   echo "-- PGI 17.4 FOUND in cache"
 else
   echo "-- PGI 17.4 NOT FOUND in cache"
   cd $HOME/Downloads
-  mkdir -p pgilinux
-  curl -L $PGI_URL | tar -xz -C pgilinux
+  curl -LO $PGI_URL
+  mv pgilinux-2017-174-x86_64.tar.gz pgilinux.tar.gz
   cd $TRAVIS_BUILD_DIR
 fi
 echo "-- Done with PGI 17.4"
 
 echo "-- Downloading MKL"
-if [[ -f $HOME/Downloads/mkl/ipsxe_support_mkl.txt ]]; then
+if [[ -f $HOME/Downloads/mkl.tar.gz ]]; then
   echo "-- MKL FOUND in cache"
 else
   echo "-- MKL NOT FOUND in cache"
   cd $HOME/Downloads
-  mkdir -p mkl
-  curl -L $MKL_URL | tar -xz -C mkl --strip-components=1
+  curl -LO $MKL_URL
+  mv l_mkl_2017.4.239.tgz mkl.tar.gz
   cd $TRAVIS_BUILD_DIR
 fi
 echo "-- Done with MKL"
 
 echo "-- Downloading Intel"
-if [[ -f $HOME/Downloads/parallel_studio/ipsxe_support_prof.txt ]]; then
+if [[ -f $HOME/Downloads/parallel_studio.tar.gz ]]; then
   echo "-- Intel FOUND in cache"
 else
   echo "-- Intel NOT FOUND in cache"
   cd $HOME/Downloads
-  mkdir -p parallel_studio
-  curl -L $INTEL_URL | tar -xz -C parallel_studio --strip-components=1
+  curl -LO $INTEL_URL
+  mv parallel_studio_xe_2018_update2_professional_edition.tgz parallel_studio.tar.gz
   cd $TRAVIS_BUILD_DIR
 fi
 echo "-- Done with Intel"
@@ -42,13 +42,12 @@ echo "-- Done with Intel"
 HDF5_VERSION="1.10.1"
 echo "-- Downloading HDF5 $HDF5_VERSION"
 HDF5_URL="https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-${HDF5_VERSION%.*}/hdf5-$HDF5_VERSION/src/hdf5-$HDF5_VERSION.tar.bz2"
-if [[ -f $HOME/Downloads/hdf5-1.10.1/COPYING_LBNL_HDF5 ]]; then
+if [[ -f $HOME/Downloads/hdf5-$HDF5_VERSION.tar.bz2 ]]; then
   echo "-- HDF5 $HDF5_VERSION FOUND in cache"
 else
   echo "-- HDF5 $HDF5_VERSION NOT FOUND in cache"
   cd $HOME/Downloads
-  mkdir -p hdf5-1.10.1
-  curl -L $HDF5_URL | tar -xj -C hdf5-1.10.1 --strip-components=1
+  curl -LO $HDF5_URL
   cd $TRAVIS_BUILD_DIR
 fi
 echo "-- Done with HDF5 $HDF5_VERSION"
