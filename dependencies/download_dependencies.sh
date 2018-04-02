7bf818e3d49514ee98b95dbf23c64cc071645735
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-04-02 13:01:02 -0400

    Small refactor

--- dependencies/download_dependencies.sh
+++ dependencies/download_dependencies.sh
@@ -3,15 +3,15 @@
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -euo pipefail
 
+cd $HOME/Downloads
+
 echo "-- Downloading PGI 17.4"
 if [[ -f $HOME/Downloads/pgilinux.tar.gz ]]; then
   echo "-- PGI 17.4 FOUND in cache"
 else
   echo "-- PGI 17.4 NOT FOUND in cache"
-  cd $HOME/Downloads
   curl -LO $PGI_URL
   mv pgilinux-2017-174-x86_64.tar.gz pgilinux.tar.gz
-  cd $TRAVIS_BUILD_DIR
 fi
 echo "-- Done with PGI 17.4"
 
@@ -20,10 +20,8 @@ if [[ -f $HOME/Downloads/mkl.tar.gz ]]; then
   echo "-- MKL FOUND in cache"
 else
   echo "-- MKL NOT FOUND in cache"
-  cd $HOME/Downloads
   curl -LO $MKL_URL
   mv l_mkl_2017.4.239.tgz mkl.tar.gz
-  cd $TRAVIS_BUILD_DIR
 fi
 echo "-- Done with MKL"
 
@@ -32,10 +30,8 @@ if [[ -f $HOME/Downloads/parallel_studio.tar.gz ]]; then
   echo "-- Intel FOUND in cache"
 else
   echo "-- Intel NOT FOUND in cache"
-  cd $HOME/Downloads
   curl -LO $INTEL_URL
   mv parallel_studio_xe_2018_update2_professional_edition.tgz parallel_studio.tar.gz
-  cd $TRAVIS_BUILD_DIR
 fi
 echo "-- Done with Intel"
 
@@ -46,8 +42,8 @@ if [[ -f $HOME/Downloads/hdf5-$HDF5_VERSION.tar.bz2 ]]; then
   echo "-- HDF5 $HDF5_VERSION FOUND in cache"
 else
   echo "-- HDF5 $HDF5_VERSION NOT FOUND in cache"
-  cd $HOME/Downloads
   curl -LO $HDF5_URL
-  cd $TRAVIS_BUILD_DIR
 fi
 echo "-- Done with HDF5 $HDF5_VERSION"
+
+cd $TRAVIS_BUILD_DIR
